b59fe459c9434ed18b660306e6bb8727b5bc9afa
ooguroiori <iori.oguro@g.dreamcareer.co.jp>
2024-09-19 18:04:26 +0900

    feat: newsページの作成

--- src/tsx/views/pages/Article.jsx
+++ src/tsx/views/pages/Article.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Header } from '../components/block/Header';
 import { Newsblock } from '../components/block/Newsblock'; 
+import "../../styles/Heading.scss";
 
 export const Article = () => {
     return (
     <div>
         <Header />
+        <h2 class="heading">記事</h2>
+        <h3 className='subtxt'>quitaや他サイトで投稿した記事を紹介します。</h3>
         <Newsblock />
     </div>
     );
